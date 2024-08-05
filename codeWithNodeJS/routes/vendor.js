71eeaf3bae019f581c283fb4262da60efa42771d
Qing <haru61410@gmail.com>
2024-08-05 20:45:23 +0800

    delete ejs part in vendor

--- codeWithNodeJS/routes/vendor.js
+++ codeWithNodeJS/routes/vendor.js
@@ -6,20 +6,6 @@ router.get("/", (req, res) => {
   res.send("vendor page");
 });
 
-// 會員資料 (for ejs)
-router.get("/profile/:vid", (req, res) => {
-  const conn = req.app.get("mysqlConnection");
-  conn.query(
-    "select * from vendor where vid = ?",
-    [req.params.vid],
-    (err, result) => {
-      res.render("vendorProfile.ejs", {
-        profile: result[0],
-        vid: req.params.vid,
-      });
-    }
-  );
-});
 // 會員資料 API
 router.get("/api/profile/:vid", (req, res) => {
   const conn = req.app.get("mysqlConnection");
@@ -32,38 +18,6 @@ router.get("/api/profile/:vid", (req, res) => {
   );
 });
 
-// 編輯會員資料 (for ejs)
-router.post("/profile/:vid", async (req, res) => {
-  try {
-    const { first_name, last_name, phone, email, address, password } = req.body;
-    const vid = req.params.vid;
-    const conn = req.app.get("mysqlConnection");
-
-    // 有被填寫的欄位才會傳入 value
-    let updateFields = {};
-    if (first_name) updateFields.first_name = first_name;
-    if (last_name) updateFields.last_name = last_name;
-    if (phone) updateFields.phone = phone;
-    if (email) updateFields.email = email;
-    if (address) updateFields.address = address;
-    // 有被填寫的密碼才會被雜湊加密並傳入
-    if (password) {
-      var hashedPW = await hashPW(password);
-      updateFields.password = hashedPW;
-    }
-
-    // 假如有欄位被填寫才會 update到資料庫，否則就是回到原畫面
-    if (Object.keys(updateFields > 0)) {
-      await updateVendorProfile(conn, vid, updateFields);
-      res.redirect(`/vendor/profile/${vid}`);
-    } else {
-      res.redirect(`/vendor/profile/${vid}`);
-    }
-  } catch (error) {
-    console.error("Error updating profile:", error);
-    res.status(500).send("An error occurred while updating the profile");
-  }
-});
 // 編輯會員資料 --React--
 router.put("/put/profile/:vid", async (req, res) => {
   try {
